bca7bf0d758bfd1eae0f21c3efa9f37fb04762c0
KT <kt@MacBook-Pro-4.local>
2019-05-10 19:35:26 +0900

    fix bag

--- leapmonth.py
+++ leapmonth.py
@@ -9,7 +9,7 @@ def main():
         print("year is more 0")
 
         return
-    if month < 0 || month > 12:
+    if month < 0 or month > 12:
         print("Error in month")
         print("month is from 1 to 12")
         print("command is 'python3 year month'")
