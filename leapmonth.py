42065b5bed53b4db7454e40ca39d57d67c3330ed
KT <kt@MacBook-Pro-4.local>
2019-05-10 19:22:14 +0900

    leapmonth #4 fixed

--- leapmonth.py
+++ leapmonth.py
@@ -3,6 +3,18 @@ import sys
 def main():
     year = int(sys.argv[1])
     month = int(sys.argv[2])
+    if year <= 0:
+        print("Error in year")
+        print("command is 'python3 year month'")
+        print("year is more 0")
+
+        return
+    if month < 0 || month > 12:
+        print("Error in month")
+        print("month is from 1 to 12")
+        print("command is 'python3 year month'")
+        
+        return
     print("Month of ", month, "/", year, " has ", 31, " days.", sep="")
 
 if __name__ == '__main__':
