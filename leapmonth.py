813d70c374984f78a100853b9e91b06b858b22e4
KT <kt@MacBook-Pro-4.local>
2019-05-10 20:05:52 +0900

    fix bag kashima

--- leapmonth.py
+++ leapmonth.py
@@ -1,19 +1,19 @@
 import sys
 
 def main():
-    year = int(sys.argv[1])
-    month = int(sys.argv[2])
+    try:
+        year = int(sys.argv[1])
+        month = int(sys.argv[2])
+    except:
+        return
+        
     if year <= 0:
-        print("Error in year")
-        print("command is 'python3 year month'")
-        print("year is more 0")
+        print("illigal inputs; usage: python leapmonth.py year month")
 
         return
     if month < 0 or month > 12:
-        print("Error in month")
-        print("month is from 1 to 12")
-        print("command is 'python3 year month'")
-        
+        print("illigal inputs; usage: python leapmonth.py year month")
+
         return
 
     d = {}
